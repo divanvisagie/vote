ec5058de5090edaeaca07151ec1281391c455a46
Divan Visagie <visagiedivan@gmail.com>
2017-03-13 20:20:02 +0200

    changes to db structure

--- src/main/resources/db/migration/V1__201703071738_create_user_schema.sql
+++ src/main/resources/db/migration/V1__201703071738_create_user_schema.sql
@@ -1,32 +1,41 @@
 CREATE TABLE "users"(
-    "id" UUID NOT NULL,
-    "login" VARCHAR NOT NULL,
-    "login_lowercase" VARCHAR NOT NULL,
+    "id" UUID NOT NULL PRIMARY KEY,
+    "username" VARCHAR NOT NULL,
     "email" VARCHAR NOT NULL NOT NULL,
     "password" VARCHAR NOT NULL NOT NULL,
     "salt" VARCHAR NOT NULL NOT NULL,
     "created_on" TIMESTAMP NOT NULL
 );
-ALTER TABLE "users" ADD CONSTRAINT "users_id" PRIMARY KEY("id");
+
+CREATE TABLE "user_roles"(
+    "id" BIGSERIAL PRIMARY KEY,
+    "name" VARCHAR NOT NULL
+)
+
+CREATE TABLE "current_user_roles"(
+    "id" BIGSERIAL PRIMARY KEY,
+    "role_id" BIGSERIAL PRIMARY KEY,
+    "user_id" UUID
+);
+ALTER TABLE "current_user_roles" ADD CONSTRAINT "current_user_roles_user_fk"
+    FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+
 
 CREATE TABLE "password_reset_codes"(
-  "id" UUID NOT NULL,
+  "id" UUID NOT NULL PRIMARY KEY,
   "code" VARCHAR NOT NULL,
   "user_id" UUID NOT NULL,
   "valid_to" TIMESTAMP NOT NULL
 );
-ALTER TABLE "password_reset_codes" ADD CONSTRAINT "password_reset_codes_id" PRIMARY KEY("id");
 ALTER TABLE "password_reset_codes" ADD CONSTRAINT "password_reset_codes_user_fk"
   FOREIGN KEY("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
-CREATE TABLE "remember_me_tokens"(
-  "id" UUID NOT NULL,
+CREATE TABLE "tokens"(
+  "id" UUID NOT NULL PRIMARY KEY,
   "selector" VARCHAR NOT NULL,
-  "token_hash" VARCHAR NOT NULL,
   "user_id" UUID NOT NULL,
   "valid_to" TIMESTAMP NOT NULL
 );
-ALTER TABLE "remember_me_tokens" ADD CONSTRAINT "remember_me_tokens_id" PRIMARY KEY("id");
-ALTER TABLE "remember_me_tokens" ADD CONSTRAINT "remember_me_tokens_user_fk"
+ALTER TABLE "tokens" ADD CONSTRAINT "tokens_user_fk"
   FOREIGN KEY("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-CREATE UNIQUE INDEX "remember_me_tokens_selector" ON "remember_me_tokens"("selector");
